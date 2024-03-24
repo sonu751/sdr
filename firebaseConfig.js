da7cbcade402039f784389dad66115f9cb00f011
sonu751 <158086083+sonu751@users.noreply.github.com>
2024-03-24 10:09:27 +0530

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB0MRiohrT-T4mcLMASLNXbU7nJL1KdBBE",
+    authDomain: "auth-b89f8.firebaseapp.com",
+    databaseURL: "https://auth-b89f8-default-rtdb.firebaseio.com",
+    projectId: "auth-b89f8",
+    storageBucket: "auth-b89f8.appspot.com",
+    messagingSenderId: "1011755334109",
+    appId: "1:1011755334109:web:0b31c1a59e7e71665f9e34"
+}
+
+const app = initializeApp(firebaseConfig);
